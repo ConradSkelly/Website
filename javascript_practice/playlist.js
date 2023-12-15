fa769ce99c650a7b138160fa74ad4d95fe4f1541
Skelly <1022892@1022892-student-fvfhx075q6l3.mynetworksettings.com>
2023-12-15 16:34:27 -0500

    added validation links

--- javascript_practice/playlist.js
+++ javascript_practice/playlist.js
@@ -1,17 +1,20 @@
 window.onload = init;
 function init() {
-    var button = document.getElementById("addButton");
+    let button = document.getElementById("addButton");
     button.onclick = handleButtonClick;
     loadPlaylist();
 }
 function handleButtonClick() {
     let textInput = document.getElementById("songTextInput"); 
-    let songName = textInput.value;
+    const songName = textInput.value;
+    console.log(songName)
     let li = document.createElement("li");
     li.innerHTML = songName;
+    console.log(songName)
     let ul = document.getElementById("playlist"); 
     ul.appendChild(li);
-    save(songName);
+    console.log(songName)
+    save("test");
     if (songName == "") {
         alert("please enter a song");
     }
--- javascript_practice/playlist_store.js
+++ javascript_practice/playlist_store.js
@@ -1,13 +1,14 @@
 function save(item) {
-    var playlistArray = getStoreArray("playlist"); playlistArray.push(item);
+    let playlistArray = getStoreArray("playlist"); 
+    playlistArray.push("test");
     localStorage.setItem("playlist", JSON.stringify(playlistArray));
 }
 function loadPlaylist() {
-    var playlistArray = getSavedSongs();
-    var ul = document.getElementById("playlist");
+    let playlistArray = getSavedSongs();
+    let ul = document.getElementById("playlist");
     if (playlistArray != null) {
-        for (var i = 0; i < playlistArray.length; i++) {
-            var li = document.createElement("li");
+        for (let i = 0; i < playlistArray.length; i++) {
+            let li = document.createElement("li");
             li.innerHTML = playlistArray[i];
             ul.appendChild(li);
         }
@@ -18,10 +19,11 @@ function getSavedSongs() {
 }
 function getStoreArray(key) {
     var playlistArray = localStorage.getItem(key);
-    if (playlistArray == null || playlistArray == "") { 
-        playlistArray = new array();
+    if (playlistArray == null || playlistArray == "") {
+        playlistArray = new Array();
     }
     else {
-    } playlistArray = JSON.parse(playlistArray); 
-    return playlistArray;
+        playlistArray = JSON.parse(playlistArray);
+        return playlistArray;
+    }
 }
